345a7c03859ed0508d055e8ee46cf4d99e84bb02
aman304gupta <aman304gupta@gmail.com>
2024-08-03 16:37:56 -0700

    Build Issues

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -146,7 +146,7 @@ export function useLocalStorage<T>(
   key: string,
   initialValue: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [value, setValue] = useState(() =>
+  const [value, setValue] = useState<T>(() =>
     JSON.parse(localStorage.getItem(key) || JSON.stringify(initialValue))
   );
 
@@ -154,7 +154,7 @@ export function useLocalStorage<T>(
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
 
-  return [value, setValue] as const;
+  return [value, setValue];
 }
 
 export function useOnClickOutside(
